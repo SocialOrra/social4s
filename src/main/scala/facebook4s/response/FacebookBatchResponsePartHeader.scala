9d531ef8a20527df30c5bc01b4cc1e79e267497f
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-01-23 11:09:06 -0500

    Removed useless method (toJson).

--- src/main/scala/facebook4s/response/FacebookBatchResponsePartHeader.scala
+++ src/main/scala/facebook4s/response/FacebookBatchResponsePartHeader.scala
@@ -4,11 +4,7 @@ import play.api.libs.json.{ JsObject, Json }
 
 /** Represents a batch response header with a name and value.
  */
-case class FacebookBatchResponsePartHeader(name: String, value: String) {
-  lazy val toJson: JsObject = Json.obj(
-    "name" -> name,
-    "value" -> value)
-}
+case class FacebookBatchResponsePartHeader(name: String, value: String)
 
 object FacebookBatchResponsePartHeader {
   implicit val batchResponsePartHeaderFmt = Json.format[FacebookBatchResponsePartHeader]
