f4ea422f6a284fcbccf6e250e54aa74c1f135880
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-04-04 07:49:50 -0400

    Deleted unused trait.

--- src/main/scala/http/client/connection/impl/PlayWSHttpConnection.scala
+++ src/main/scala/http/client/connection/impl/PlayWSHttpConnection.scala
@@ -21,10 +21,6 @@ private[impl] object PlayWsHttpResponse {
   def apply(wsReponse: WSResponse): PlayWsHttpResponse = PlayWsHttpResponse(wsReponse.status, wsReponse.allHeaders, wsReponse)
 }
 
-trait RequestsPerSecond extends HttpConnection {
-
-}
-
 class PlayWSHttpConnection extends HttpConnection {
 
   val GET = "GET"
