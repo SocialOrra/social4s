5053844cd78a08540249fcf814d116f086568d4c
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-04-13 12:20:33 -0400

    Added core logic for TwitterRequestBuilder fetching and paging requests.

--- twitter4s/src/main/scala/twitter4s/TwitterRequestBuilder.scala
+++ twitter4s/src/main/scala/twitter4s/TwitterRequestBuilder.scala
@@ -3,6 +3,7 @@ package twitter4s
 import http.client.connection.HttpConnection
 import http.client.request.{GetRequest, PostRequest, Request}
 import http.client.response.{BatchResponsePart, HttpResponse}
+import play.api.http.Writeable
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -10,89 +11,68 @@ import play.api.libs.json.JsValue
 
 trait TwitterResponsePart extends BatchResponsePart
 
-trait TwitterResponse extends HttpResponse {
-  val code: Int
-}
-
 abstract class TwitterRequestBuilder(connection: HttpConnection, batchUrl: String) {
 
-  protected def fromHttpResponse(response: HttpResponse): TwitterResponsePart
   protected def maybePaginated[T <: Request](paginated: Boolean, request: T): T
-  protected def accumulateCompleteRequest(request: Request, response: TwitterResponsePart): (Request, TwitterResponsePart)
+  protected def extractResponsePart(request: Request, respone: HttpResponse): TwitterResponsePart
   protected def newRequestFromIncompleteRequest(request: Request, response: TwitterResponsePart): Request
   protected def maybeRanged[T <: Request](since: Option[Long], until: Option[Long], request: T): T
 
   def shutdown() = connection.shutdown()
 
-  def get(getRequest: GetRequest, since: Option[Long], until: Option[Long]): Future[TwitterResponse] = {
+  def get(getRequest: GetRequest, since: Option[Long], until: Option[Long]): Future[(Request, Seq[TwitterResponsePart])] = {
     val r = maybeRanged(since, until, getRequest)
-    executeWithPagination(r)
+    val f = connection.get(r)
+    executeWithPagination(r, f)
   }
 
-  def get(getRequest: GetRequest, paginate: Boolean): Future[TwitterResponse] = {
+  def get(getRequest: GetRequest, paginate: Boolean): Future[(Request, Seq[TwitterResponsePart])] = {
     val r = maybePaginated(paginate, getRequest)
-    executeWithPagination(r)
+    val f = connection.get(r)
+    executeWithPagination(r, f)
   }
 
-  def post[T](postRequest: PostRequest[T], since: Option[Long], until: Option[Long]): Future[TwitterResponse] = {
+  def post[T](postRequest: PostRequest[T], since: Option[Long], until: Option[Long])(implicit writeable: Writeable[T]): Future[(Request, Seq[TwitterResponsePart])] = {
     val r = maybeRanged(since, until, postRequest)
-    executeWithPagination(r)
+    val f = connection.post(r)
+    executeWithPagination(r, f)
   }
 
-  def post[T](postRequest: PostRequest[T], paginated: Boolean)(implicit ec: ExecutionContext): Future[TwitterResponse] = {
+  def post[T](postRequest: PostRequest[T], paginated: Boolean)(implicit writeable: Writeable[T]): Future[(Request, Seq[TwitterResponsePart])] = {
     val r = maybePaginated(paginated, postRequest)
-    executeWithPagination(r)
+    val f = connection.post(r)
+    executeWithPagination(r, f)
   }
 
-  def executeWithPagination(request: Request)(implicit ec: ExecutionContext): Future[TwitterResponse] = {
-    val f = _executeWithPagination(request) map { requestsAndResponses ⇒
-      requestsAndResponses
-        // group response parts by request
-        .groupBy(_._1)
-        // remove grouping key, leave (request,responseParts)
-        .mapValues(_.map(_._2))
-        .map { requestAndResponseParts ⇒
-          // TODO: could there be no parts?
-          val request = requestAndResponseParts._1
-          val parts = requestAndResponseParts._2
-          //val combinedBody: String = parts.map(p ⇒ p.bodyJson.validate[JsObject].get).foldLeft(JsObject(Seq.empty))(_ deepMerge _).toString()
-          //val combinedPart = TwitterResponsePart(code = parts.head.code, headers = parts.head.headers, body = combinedBody)
-          (request, parts)
-        }
+  def executeWithPagination(request: Request, responseF: Future[HttpResponse])(implicit ec: ExecutionContext): Future[(Request, Seq[TwitterResponsePart])] = {
+    val f = _executeWithPagination(request, responseF) map { requestAndResponseParts ⇒
+      // TODO: could there be no parts?
+      val request = requestAndResponseParts._1
+      val parts = requestAndResponseParts._2
+      (request, parts)
     }
-
-    postExecute()
     f
-
-    ???
   }
 
-  private def postExecute(): Unit = {
-  }
+  private def _executeWithPagination(request: Request, responseF: Future[HttpResponse], completedResponseParts: Seq[TwitterResponsePart] = Seq.empty)(implicit ec: ExecutionContext): Future[(Request, Seq[TwitterResponsePart])] = {
 
-  private def _executeWithPagination(request: Request, completedRequests: Seq[(Request, TwitterResponsePart)] = Seq.empty)(implicit ec: ExecutionContext): Future[Seq[(Request, TwitterResponsePart)]] = {
+    responseF.map { response =>
 
-    val f: Future[HttpResponse] = if (request.isInstanceOf[GetRequest]) connection.get(request.asInstanceOf[GetRequest])
-    else connection.post(request.asInstanceOf[PostRequest[Array[Byte]]])
 
-    f.map { rawResponse =>
+      val responsePart = extractResponsePart(request, response)
 
-      val response = fromHttpResponse(rawResponse)
-
-      val complete = accumulateCompleteRequest(request, response)
-
-      val newRequest = if (!isRequestComplete(request, response))
-        Some(newRequestFromIncompleteRequest(request, response))
+      val newRequest = if (!isRequestComplete(request, responsePart))
+        Some(newRequestFromIncompleteRequest(request, responsePart))
       else None
 
-      (complete, newRequest)
+      (responsePart, newRequest)
 
     } flatMap {
-      case (complete, Some(newRequest)) ⇒
-        //_executeWithPagination(newRequest, completedRequests ++ complete)
+      case (responsePart, Some(newRequest)) ⇒
+        //_executeWithPagination(newRequest, completedResponseParts ++ Seq(responsePart))
         ???
-      case (complete, None) ⇒
-        Future.successful { completedRequests /*++ complete*/ }
+      case (responsePart, None) ⇒
+        Future.successful { (request, completedResponseParts ++ Seq(responsePart)) }
     }
   }
 
