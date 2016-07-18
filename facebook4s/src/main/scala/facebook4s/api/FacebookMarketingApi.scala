0a6846151a2e50b430e114f552f42e3bcb67f06b
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-07-18 11:10:04 -0400

    Added more FB API calls. - adSets - ads - pageInsights - pagePosts

--- facebook4s/src/main/scala/facebook4s/api/FacebookMarketingApi.scala
+++ facebook4s/src/main/scala/facebook4s/api/FacebookMarketingApi.scala
@@ -8,17 +8,6 @@ object FacebookMarketingApi extends HttpRequestHelpers {
   import FacebookApiConstants._
 
   implicit class FacebookAdsInsightsApiImplicits(requestBuilder: FacebookBatchRequestBuilder) {
-    def adInsights(
-      adId:        String,
-      metric:      Option[String]      = None,
-      period:      Option[String]      = None,
-      since:       Option[Long]        = None,
-      until:       Option[Long]        = None,
-      accessToken: Option[AccessToken] = None) = {
-      val relativeUrl = buildRelativeUrl(adId, "insights", metric)
-      val modifiers = buildModifiers("period" → period)
-      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), since, until)
-    }
 
     def adAccount(
       adAccountId: String,
@@ -29,23 +18,85 @@ object FacebookMarketingApi extends HttpRequestHelpers {
       requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), paginated = true)
     }
 
-    def adCampaigns(
-      adAccountId: String,
-      limit:       Option[Int]         = None,
-      accessToken: Option[AccessToken] = None) = {
-      val relativeUrl = buildRelativeUrl(s"v2.6/act_$adAccountId/campaigns")
-      val modifiers = buildModifiers(
-        "limit" → limit,
-        "fields" → Some("id,name,account_id,buying_type,adlabels,objective,start_time,stop_time"))
-      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), paginated = true)
-    }
-
     def adCampaign(
       adCampaignId: String,
       accessToken:  Option[AccessToken] = None) = {
       val relativeUrl = buildRelativeUrl(adCampaignId)
       val modifiers = buildModifiers("fields" → Some("name,created_time"))
       requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), paginated = false)
+    }
+
+    def adCampaigns(
+      adAccountId: String,
+      limit:       Option[Int]         = None,
+      accessToken: Option[AccessToken] = None) = {
+      val relativeUrl = buildRelativeUrl("v2.6", s"act_$adAccountId", "campaigns")
+      val modifiers = buildModifiers(
+        "limit" → limit,
+        "fields" → Some("id,name,account_id,buying_type,adlabels,objective,start_time,stop_time"))
+      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), paginated = true)
+    }
+
+    def adSets(
+      campaignId:  String,
+      limit:       Option[Int]         = None,
+      accessToken: Option[AccessToken] = None) = {
+      val relativeUrl = buildRelativeUrl(campaignId, "adsets")
+      val modifiers = buildModifiers(
+        "limit" → limit,
+        "fields" → Some("id,name,account_id,start_time,budget_remaining,created_time,bid_amount,billing_event,configured_status,daily_budget,effective_status"))
+      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), paginated = true)
+    }
+
+    def ads(
+      adSetId:     String,
+      limit:       Option[Int]         = None,
+      accessToken: Option[AccessToken] = None) = {
+      val relativeUrl = buildRelativeUrl(adSetId, "ads")
+      val modifiers = buildModifiers(
+        "limit" → limit,
+        "fields" → Some("adlabels,bid_amount,bid_info,bid_type,configured_status,conversion_specs,name,tracking_specs,created_time,adset_id,effective_status,status,creative{id,thumbnail_url,object_type,object_story_spec}"))
+      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), paginated = true)
+    }
+
+    def adInsights(
+      adId:        String,
+      metric:      Option[String]      = None,
+      period:      Option[String]      = None,
+      since:       Option[Long]        = None,
+      until:       Option[Long]        = None,
+      accessToken: Option[AccessToken] = None) = {
+      val relativeUrl = buildRelativeUrl(adId, "insights", metric)
+      val modifiers = buildModifiers(
+        "period" → period,
+        "time_increment" → Some("1"))
+      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), since, until)
+    }
+
+    def pageInsights(
+      pageId:      String,
+      metric:      Option[String]      = None,
+      period:      Option[String]      = None,
+      since:       Option[Long]        = None,
+      until:       Option[Long]        = None,
+      accessToken: Option[AccessToken] = None) = {
+      val relativeUrl = buildRelativeUrl(pageId, "insights", metric)
+      val modifiers = buildModifiers(
+        "period" → period)
+      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), since, until)
+    }
+
+    def pagePosts(
+      pageId:      String,
+      period:      Option[String]      = None,
+      since:       Option[Long]        = None,
+      until:       Option[Long]        = None,
+      accessToken: Option[AccessToken] = None) = {
+      val relativeUrl = buildRelativeUrl(pageId, "posts")
+      val modifiers = buildModifiers(
+        "period" → period,
+        "fields" → Some("message,link,id,call_to_action,attachments,created_time"))
+      requestBuilder.add(FacebookGetRequest(relativeUrl, None, Seq.empty, modifiers, accessToken), since, until)
     }
   }
 }
